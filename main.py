7f181ea36a87bd452ead2632a550b16a4970437d
Jenni M <jennifermoulder17@gmail.com>
2021-08-16 12:36:13 +0100

    Basic class creation

---
+++ main.py
@@ -0,0 +1,18 @@
+class Outfit:
+    def __init__(self, top, trousers):
+        self.top = top 
+        self.trousers = trousers
+
+    def addTop(self):
+        pass
+
+    def addBottoms(self):
+        pass
+
+
+
+#create interface
+#add manequin
+#add something onto manequin
+#add button to add 
+#add more options and colours
